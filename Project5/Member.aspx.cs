96e179989b433cd265b580b662062f96f24f94ea
Jared <jaredlham@gmail.com>
2020-11-21 08:38:00 -0700

    added check if user is authenticated

--- Project5/Member.aspx.cs
+++ Project5/Member.aspx.cs
@@ -10,16 +10,44 @@ namespace Project5
 {
     public partial class Member : System.Web.UI.Page
     {
+
+        private FormsAuthenticationTicket userTicket;
         protected void Page_Load(object sender, EventArgs e)
         {
             // Implement our own cookies to auto authenticate members
-            // if(! alreadyLoggedin){
-            Response.Redirect("~/MemberLogin.aspx");
+            if (!this.isMemberLoggedIn())
+            {
+                
+                Response.Redirect("~/MemberLogin.aspx");
+            }
         }
 
         protected void homePageButton_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Default.aspx");
+        }
+
+        private bool isMemberLoggedIn()
+        {
+            try
+            {
+                HttpCookie authCookie = Request.Cookies.Get("memberCookie");
+                if(authCookie == null)
+                {
+                    return false;
+                }
+                userTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            } catch (ArgumentException)
+            {
+                return false;
+            }
+            
+            if(userTicket == null)
+            {
+                return false;
+            }
+            return true;
+                 
         }
     }
 }
